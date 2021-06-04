8632f20658376a342946809fcc256e03c8aeb9ab
Ratatou2 <ws4232@naver.com>
2021-06-04 11:28:32 +0900

    선형 탐색 구현

---
+++ Algorithm/Linear_search.py
@@ -0,0 +1,17 @@
+# 방법 1
+def linear_search(element, some_list):
+    for i in some_list:
+        if element == i:
+            return some_list.index(i)
+
+# # 방법 2
+# def linear_search(element, some_list):
+#     for i in range(len(some_list)):
+#         if element == some_list[i]:
+#             return i
+
+print(linear_search(2, [2, 3, 5, 7, 11]))
+print(linear_search(0, [2, 3, 5, 7, 11]))
+print(linear_search(5, [2, 3, 5, 7, 11]))
+print(linear_search(3, [2, 3, 5, 7, 11]))
+print(linear_search(11, [2, 3, 5, 7, 11]))
