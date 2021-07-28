5ed0d688084ac83881a38eb70879077625cbaff3
Ratatou2 <Ratatou2@users.noreply.github.com>
2021-07-28 16:08:50 +0900

    공부 필요

---
+++ Algorithm/staircase.py
@@ -0,0 +1,33 @@
+fib_cache={}
+def staircase(n):
+    if n<2:
+        return 1
+    if n in fib_cache:
+        return fib_cache[n]
+    else:
+        temp=staircase(n-1) + staircase(n-2)
+        fib_cache[n] = temp
+        return temp
+
+
+# 테스트
+print(staircase(0))
+print(staircase(6))
+print(staircase(15))
+print(staircase(25))
+print(staircase(41))
+
+
+# 해답코드
+def staircase(n):
+    a, b = 1, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+
+# 테스트
+print(staircase(0))
+print(staircase(6))
+print(staircase(15))
+print(staircase(25))
+print(staircase(41))
