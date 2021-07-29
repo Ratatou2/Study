2bfb4138b9ad58873e9534d291186a3ec21aca38
Ratatou2 <Ratatou2@users.noreply.github.com>
2021-07-29 11:21:20 +0900

    공부 필요

---
+++ Algorithm/staircase+.py
@@ -0,0 +1,23 @@
+# 높이 n개의 계단을 올라가는 방법을 리턴한다
+def staircase(stairs, possible_steps):
+    # 계단 높이가 0 이거나 1 이면 올라가는 방법은 한 가지밖에 없다
+    number_of_ways = [1, 1]
+
+    # 이 변수들을 업데이트 해주며 n 번째 계단을 오르는 방법의 수를 구한다.
+    for height in range(2, stairs + 1):
+        number_of_ways.append(0)
+
+        for step in possible_steps:
+            # 음수 계단 수는 존재하지 않기 때문에 무시합니다
+            if height - step >= 0:
+                number_of_ways[height] += number_of_ways[height - step]
+                print(number_of_ways, number_of_ways[height], number_of_ways[height - step])
+
+    return number_of_ways[stairs]
+
+
+# 테스트 / 출근 3
+print(staircase(5, [1, 2, 3]))
+print(staircase(6, [1, 2, 3]))
+print(staircase(7, [1, 2, 4]))
+print(staircase(8, [1, 3, 5]))
